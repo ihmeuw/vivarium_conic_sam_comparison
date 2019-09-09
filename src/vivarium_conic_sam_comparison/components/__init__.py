62c918e5d9298efffb32b51dcdea5d708ed6ff50
Cody <chorst@uw.edu>
2019-09-09 10:41:03 -0700

    move sample history observer into metrics

--- src/vivarium_conic_sam_comparison/components/__init__.py
+++ src/vivarium_conic_sam_comparison/components/__init__.py
@@ -3,5 +3,4 @@ from .lbwsg import LBWSGRisk, LBWSGRiskEffect
 from .neonatal_preterm import NeonatalPreterm
 from .treatment import MaternalTreatmentAlgorithm, NeonatalTreatmentAlgorithm
 from .effect import InterventionEffect
-from .observer import SampleHistoryObserver
 from . import metrics
